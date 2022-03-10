cd6ad64e6d3948b6beb55a51d242a555884c9fbc
Derek McEnroe <derek.mcenroe@gmail.com>
2022-03-10 15:27:22 -0600

    Add GUI to play single rounds and tally score

--- rps-game.js
+++ rps-game.js
@@ -1,4 +1,7 @@
-console.log(playRound());
+
+
+
+
 
 //get a random number to randomize computer rock paper scissors selection (1-3)
 function getRandNum(min,max){
@@ -14,11 +17,11 @@ function computerTurn() {
 
     switch (getRandNum(1,3)) {
         case 1:
-            return "rock";
+            return "Rock";
         case 2:
-            return "paper";
+            return "Paper";
         case 3:
-            return "scissors";  
+            return "Scissors";  
         }
 }
 
@@ -27,72 +30,88 @@ function computerTurn() {
 //rock beats scissors, scissors beats paper, paper beats rock
 function playGame() {
 
-    let humanPlay = window.prompt("We're playing Rock, Paper, Scissors.  Pick one to play:");
-        humanPlay = humanPlay.toLowerCase();
+    let humanPlay = this.innerText;
     let computerPlay = computerTurn();
     let gameResult;
+    let currPlayerScore = Number(document.querySelector(".playerWinCounter").innerText);
+    let currComputerScore = Number(document.querySelector(".computerWinCounter").innerText);
+    console.log(currPlayerScore);
 
-    if (humanPlay == computerPlay) {
+    if (humanPlay === computerPlay) {
         gameResult = "TIE"
-    } else if (humanPlay == "rock" && computerPlay !== "paper") {
+    } else if (humanPlay === "Rock" && computerPlay !== "Paper") {
+        gameResult = "Human Wins";
+        currPlayerScore = currPlayerScore + 1;
+    } else if (humanPlay === "Scissors" && computerPlay !== "Rock") { 
+        gameResult = "Human Wins";
+        currPlayerScore = currPlayerScore + 1;
+    } else if (humanPlay === "Paper" && computerPlay !== "Scissors") {
         gameResult = "Human Wins"
-    } else if (humanPlay == "scissors" && computerPlay !== "rock") { 
-        gameResult = "Human Wins"
-    } else if (humanPlay == "paper" && computerPlay !== "scissors") {
-        gameResult = "Human Wins"
-    } else gameResult = "Computer wins"
+        currPlayerScore = currPlayerScore + 1;
+    } else if (gameResult = "Computer wins") {
+        currComputerScore = currComputerScore + 1;
+    }
+
+    document.querySelector(".playerSelection").innerText = humanPlay;
+    document.querySelector(".computerSelection").innerText = computerPlay;
+    document.querySelector(".gameResult").innerText = `${gameResult}!`;
+    document.querySelector(".playerWinCounter").innerText = currPlayerScore;
+    document.querySelector(".computerWinCounter").innerText = currComputerScore;
+
 
     return [gameResult, computerPlay, humanPlay];
 }
 
-
+const playerSelection = document.querySelectorAll(".gameButton");
+console.log(playerSelection);
+playerSelection.forEach(gameButton => gameButton.addEventListener('click', playGame));
 
 //plays a round of 5 games
 
 
-function playRound() {
-    let computerScore = 0;
-    let humanScore = 0;
-    let gameResultArray, winner, loser, winnerScore, loserScore, resolveText, humanPlay, computerPlay, gameResult;
+// function playRound() {
+//     let computerScore = 0;
+//     let humanScore = 0;
+//     let gameResultArray, winner, loser, winnerScore, loserScore, resolveText, humanPlay, computerPlay, gameResult;
     
 
-    for (let i = 1; i <= 5; i++) {
+//     for (let i = 1; i <= 5; i++) {
 
-        gameResultArray = playGame();
-            humanPlay = gameResultArray[2];
-            computerPlay = gameResultArray[1];
-            gameResult = gameResultArray[0];
+//         gameResultArray = playGame();
+//             humanPlay = gameResultArray[2];
+//             computerPlay = gameResultArray[1];
+//             gameResult = gameResultArray[0];
 
-        if (gameResult !== "TIE") {
-            if (gameResult == "Computer wins") {
-            computerScore++;
-            } else humanScore++;
-        }
-        console.log(`Game ${i}:
-        Human plays: ${humanPlay}
-        Computer plays: ${computerPlay}
-        ${gameResult}!
-        Current Score: Human: ${humanScore} - Computer: ${computerScore}`)
-    }
+//         if (gameResult !== "TIE") {
+//             if (gameResult == "Computer wins") {
+//             computerScore++;
+//             } else humanScore++;
+//         }
+//         console.log(`Game ${i}:
+//         Human plays: ${humanPlay}
+//         Computer plays: ${computerPlay}
+//         ${gameResult}!
+//         Current Score: Human: ${humanScore} - Computer: ${computerScore}`)
+//     }
 
-    if (computerScore == humanScore) {
-        resolveText = "It's a TIE!"
-        winner = "Human";
-        loser = "Computer";
-        winnerScore = humanScore;
-        loserScore = computerScore;
-    } else if (computerScore > humanScore) {
-        resolveText = "Computer wins!"
-        winner = "Computer";
-        loser = "Human";
-        winnerScore = computerScore;
-        loserScore = humanScore;
-    } else {
-        resolveText = "Human wins!"
-        winner = "Human";
-        loser = "Computer";
-        winnerScore = humanScore;
-        loserScore = computerScore;
-    }
-    return (`${resolveText}  Final Score:  ${winner}: ${winnerScore} - ${loser}: ${loserScore}`);
-    }
+//     if (computerScore == humanScore) {
+//         resolveText = "It's a TIE!"
+//         winner = "Human";
+//         loser = "Computer";
+//         winnerScore = humanScore;
+//         loserScore = computerScore;
+//     } else if (computerScore > humanScore) {
+//         resolveText = "Computer wins!"
+//         winner = "Computer";
+//         loser = "Human";
+//         winnerScore = computerScore;
+//         loserScore = humanScore;
+//     } else {
+//         resolveText = "Human wins!"
+//         winner = "Human";
+//         loser = "Computer";
+//         winnerScore = humanScore;
+//         loserScore = computerScore;
+//     }
+//     return (`${resolveText}  Final Score:  ${winner}: ${winnerScore} - ${loser}: ${loserScore}`);
+//     }
