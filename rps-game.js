453ef5945bf2c5167459ff12188c97c4b2e62287
Derek McEnroe <derek.mcenroe@gmail.com>
2022-03-18 11:41:32 -0500

    Finish project -- still more that could be done

--- rps-game.js
+++ rps-game.js
@@ -24,16 +24,12 @@ function computerTurn() {
 function getGameResult(game) {
     if (game.humanPlay === game.computerPlay) {
         return gameResult = "TIE"
-
     } else if (game.humanPlay === "Rock" && game.computerPlay !== "Paper") {
         return gameResult = "Human Wins";
-
     } else if (game.humanPlay === "Scissors" && game.computerPlay !== "Rock") { 
         return gameResult = "Human Wins";
-
     } else if (game.humanPlay === "Paper" && game.computerPlay !== "Scissors") {
         return gameResult = "Human Wins"
-
     } else  {
         return gameResult = "Computer Wins"
     }
@@ -41,8 +37,8 @@ function getGameResult(game) {
 
 function getPoints(game) {
 
-    //array[0] = computer point to add
-    //array[1] = human point to add
+    //array[0] = computer point to.toggle
+    //array[1] = human point to.toggle
     switch (game.gameResult) {
         case "TIE":
              return;
@@ -55,58 +51,139 @@ function getPoints(game) {
     }
 }
 
-
-function updateRoundGraphic(game) {
+function updateRoundGraphic(game, keyPress) {
 
     let currGameItem;
     let gameCompleteStyle;
-    
+    let computerSelectionBox = document.querySelector(".computerSelectionBox");
+
     if (game.gameResult == 'Human Wins') {
         gameCompleteStyle = 'gameCompleteHuman';
+        keyPress.firstElementChild.classList.add("check");
+        computerSelectionBox.firstElementChild.classList.add("x");
     } else if (game.gameResult == 'Computer Wins') {
         gameCompleteStyle = 'gameCompleteComputer';
+        keyPress.firstElementChild.classList.add("x");
+        computerSelectionBox.firstElementChild.classList.add("check");
     } else if (game.gameResult == 'TIE') {
         gameCompleteStyle = 'gameCompleteTie';
+        keyPress.firstElementChild.classList.add("x");
+        computerSelectionBox.firstElementChild.classList.add("x");
     }
     currGameItem = document.querySelector(".game" + `${game.roundNumber+1}`);
+    currGameItem.innerText = "";
     currGameItem.classList.toggle(gameCompleteStyle);
+
+        //disable buttons at end of round
+        let buttons = document.querySelectorAll(".gameButton");
+        buttons.forEach(gameButton => gameButton.disabled = true);
+        buttons.forEach(gameButton => gameButton.classList.toggle("disabledButton"));
+
+        //toggle overlay at end of game
+        setTimeout(() => toggleOverlay(game), 400);
+
 }
 
-function updateText(game) {
-    document.querySelector(".playerSelection").innerText = game.humanPlay;
-    document.querySelector(".computerSelection").innerText = game.computerPlay;
-    document.querySelector(".gameResult").innerText = `${game.gameResult}!`;
+function toggleOverlay(game){
+    let container = document.querySelector(".container");
+    let overlay = document.createElement("div");
+    let newGameBtn = document.createElement("button");
+    let gameRoundText = document.createElement("div");
+
+    overlay.classList.toggle("overlay")
+    container.append(overlay);
+
+    gameRoundText.innerText = game.gameResult + "!";
+    gameRoundText.classList.toggle("gameRoundText");
+    overlay.append(gameRoundText);
+
+    newGameBtn.classList.toggle("newGameButton");
+    newGameBtn.innerText = "Next Round";
+    overlay.append(newGameBtn);
+
+        //reset a game when reset button is pushed
+        let nextGameButton = document.querySelector(".newGameButton");
+        nextGameButton.addEventListener('click', newRound);
+}
+
+function updateText(game, keyPress) {
+
+    let versus = document.createElement("div");
+    versus.classList.toggle("versus");
+    versus.innerText = "vs.";
+
+    //toggle gui element for the computers selection below the players selection
+    let computerSelectionBox = document.createElement("div");
+    computerSelectionBox.classList.toggle("computerSelectionBox");
+    computerSelectionBox.innerText = game.computerPlay;
+    keyPress.closest('div').append(versus);
+    keyPress.closest('div').append(computerSelectionBox);
+
+    //toggle a div for the computer's check mark or x to go in
+    let winLoseIndicator = document.createElement("winLoseIndicator");
+    computerSelectionBox.appendChild(winLoseIndicator);
+
+    //print game result below the two gui elements
+    // let gameResultText = document.createElement("div");
+    // gameResultText.classList.toggle("gameResultText");
+    // gameResultText.innerText = game.gameResult + "!";
+    // computerSelectionBox.parentElement.append(gameResultText);
+
     document.querySelector(".playerWinCounter").innerText = game.currPlayerScore;
     document.querySelector(".computerWinCounter").innerText = game.currComputerScore;
-    document.querySelector(".roundNumber").innerText = game.roundNumber +1;
+
 }
 
-function playGame(keyPress) {
+function playGame(keyPress, game) {
 
-   //establish current scores and round number before the game is played
-    let currPlayerScore = Number(document.querySelector(".playerWinCounter").innerText);
-    let currComputerScore = Number(document.querySelector(".computerWinCounter").innerText);
-    let roundNumber = Number(document.querySelector(".roundNumber").innerText);
-
+    // keyPress.classList.toggle("buttonPressed");
+    //document.querySelector().style.backgroundColor = 'red';
 
 
     //get the plays and get game result
-    let humanPlay = keyPress;
+    let humanPlay = keyPress.innerText;
     let computerPlay = computerTurn();
-    let game = {humanPlay:humanPlay, computerPlay:computerPlay, currPlayerScore:currPlayerScore, currComputerScore:currComputerScore
-                , roundNumber:roundNumber, gameResult:null};
     
+    game.humanPlay = humanPlay;
+    game.computerPlay = computerPlay;
+
+
     game.gameResult = getGameResult(game);
 
-    getPoints(game);            //get the points earned from the game result and add it to each players current score
-    updateText(game);           //update ui text displays
-    updateRoundGraphic(game);   //update graphical round display
+    getPoints(game);            //get the points earned from the game result and.toggle it to each players current score
+    updateText(game, keyPress);           //update ui text displays
+    updateRoundGraphic(game, keyPress);   //update graphical round display
+
+    game.roundNumber += 1;
+    checkRound();
 
     return game;
 }
 
+function newRound() {
+    let overlay = document.querySelector(".overlay");
+    let computerSelectionBox = document.querySelector(".computerSelectionBox");
+    let versus = document.querySelector(".versus");
+    let winLoseIndicator = document.querySelectorAll(".winLoseIndicator");
+    let gameButton = document.querySelectorAll(".gameButton");
+        
+        // gameResultText.remove();
+        versus.remove();
+        overlay.remove();
+        computerSelectionBox.remove();
+        winLoseIndicator.forEach(removeIndicator => removeIndicator.classList.remove("check"));
+        winLoseIndicator.forEach(removeIndicator => removeIndicator.classList.remove("x"));
+        gameButton.forEach(removeStyle => removeStyle.classList.remove("buttonPressed"));
+
+
+        //enable buttons
+        let buttons = document.querySelectorAll(".gameButton");
+        buttons.forEach(gameButton => gameButton.disabled = false);
+        buttons.forEach(gameButton => gameButton.classList.toggle("disabledButton"));
+}
+
 function resetGame() {
-    hiddenElements = document.querySelectorAll(".hide-element");
+         hiddenElements = document.querySelectorAll(".hide-element");
     
         for (let i = 0; i < hiddenElements.length; i++) {
             hiddenElements[i].classList.toggle("hide-element");
@@ -123,61 +200,78 @@ function resetGame() {
         newGameButton.classList.toggle("hide-element");
         newGameButton.remove();
 
-        document.querySelector(".playerSelection").innerText = "";
-        document.querySelector(".computerSelection").innerText = "";
-        document.querySelector(".gameResult").innerText = "";
-        document.querySelector(".playerWinCounter").innerText = 0;
-        document.querySelector(".computerWinCounter").innerText = 0;
-        document.querySelector(".roundNumber").innerText = 0;
+        document.querySelector(".playerWinCounter").innerText = "0";
+        document.querySelector(".computerWinCounter").innerText = "0";
+
+        game = {humanPlay:"", computerPlay:"", currPlayerScore:0, currComputerScore:0
+        , roundNumber:0, gameResult:null};
 
         let allCircles = document.querySelectorAll(".visualRoundItem");
             allCircles.forEach(visualRoundItem => visualRoundItem.classList.remove("gameCompleteHuman"));
             allCircles.forEach(visualRoundItem => visualRoundItem.classList.remove("gameCompleteComputer"));
             allCircles.forEach(visualRoundItem => visualRoundItem.classList.remove("gameCompleteTie"));
 
+        document.querySelector(".game1").innerText = "1";
+        document.querySelector(".game2").innerText = "2";   
+        document.querySelector(".game3").innerText = "3";   
+        document.querySelector(".game4").innerText = "4";   
+        document.querySelector(".game5").innerText = "5";   
 
+        newRound();
 
 }
 
 function checkRound() {
 
-    let roundNumber = Number(document.querySelector(".roundNumber").innerText);
 
-if (roundNumber !== -1) {
+if (game.roundNumber !== -1) {
 
-    if (roundNumber == 5) {
+    if (game.roundNumber == 5) {
         roundContainerChildren = document.querySelector(".roundContainer").children;
     
         for (let i = 0; i < roundContainerChildren.length; i++) {
             roundContainerChildren[i].classList.toggle("hide-element");
         }
-
-        roundNumberElement = document.querySelector(".roundNumber");
-        roundNumberElement.innerText = -1;
-        roundNumberElement.classList.add("hide-element");
     
         roundContainer = document.querySelector(".roundContainer");
         roundContainer.classList.toggle("endOfRound")
 
-        let endOfRoundContent = document.createElement("div");
-        endOfRoundContent.classList.add("endOfRoundContent");
+        let endOfRoundContent = document.createElement("h3");
+        endOfRoundContent.classList.toggle("endOfRoundContent");
         endOfRoundContent.innerText = "End of Round!"
         roundContainer.appendChild(endOfRoundContent);
 
         let newGameButton = document.createElement("button");
         newGameButton.innerText = "Play Again";
-        newGameButton.classList.add("resetButton");
+        newGameButton.classList.toggle("resetButton");
         roundContainer.appendChild(newGameButton);
         resetButton = document.querySelector(".resetButton");
         resetButton.addEventListener('click', resetGame);
 
 
-    } else playGame(this.innerText);
+    } else playGame(this, game);
 }
 }
+
+function createGameSession() {
+
+    let game = {humanPlay:"", computerPlay:"", currPlayerScore:0, currComputerScore:0
+    , roundNumber:0, gameResult:null};
+
+    Number(document.querySelector(".playerWinCounter").innerText = "0");
+    Number(document.querySelector(".computerWinCounter").innerText = "0");
+
+    return game;
+}
+
+ 
+let game = createGameSession();
 
 //start a game when player clicks a button
 let playerSelection = document.querySelectorAll(".gameButton");
 playerSelection.forEach(gameButton => gameButton.addEventListener('click', checkRound));
 
-//reset a game when reset button is pushed
+
+
+
+
